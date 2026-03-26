14103fd8b8b43f60dbc90316fc01e0cf2e4c78c8
Neil-Sahay <ns8497@srmist.edu.in>
2026-03-26 08:55:32 +0530

    Add all Level 1 solutions (15 problems)

---
+++ level1/q15.java
@@ -0,0 +1,17 @@
+﻿import java.util.Scanner;
+
+class FactorialFor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int num = input.nextInt();
+        if (num <= 0) {
+            System.out.println("Please enter a positive integer.");
+        } else {
+            int fact = 1;
+            for (int i = 1; i <= num; i++) fact *= i;
+            System.out.println("Factorial of " + num + " is " + fact);
+        }
+        input.close();
+    }
+}
---
+++ level1/q9.java
@@ -0,0 +1,13 @@
+﻿import java.util.Scanner;
+
+class CountdownFor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter countdown start: ");
+        int start = input.nextInt();
+        for (int i = start; i >= 1; i--) {
+            System.out.println(i);
+        }
+        input.close();
+    }
+}
